bbaed930a1a3386029804febebf5bad3517bc3e2
danushka <danmadushzxc@gmail.com>
2026-06-03 16:56:22 +0530

    refactor:added common module

--- My-Portfolio/src/app/shared/components/project-card/project-card.component.ts
+++ My-Portfolio/src/app/shared/components/project-card/project-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Project } from '../../../core/models/project.model';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
